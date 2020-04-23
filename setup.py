2a32e83486a79d5bbdc83706fc452e8532f2c0ae
Iftekhar Hussain <iftee.husain@hotmail.co.uk>
2020-04-23 05:15:53 +0000

    Initial Commit

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import find_packages, setup
+
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
+setup(
+    name='pgpackup',
+    version='0.1.0',
+    author='Iftekhar Hussain',
+    author_email='iftee.hussain@hotmail.co.uk',
+    description='A utility for backing up PostgreSQL databases',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/hussainiftee/pgbackup',
+    packages=find_packages('src')
+    )
